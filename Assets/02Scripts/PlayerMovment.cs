3081ba916396114263448eefe5d0a9a25853e0e5
Dkirkovik <d.kirkovik@gmail.com>
2022-02-10 04:17:06 +0100

    build game

--- Assets/02Scripts/PlayerMovment.cs
+++ Assets/02Scripts/PlayerMovment.cs
@@ -11,9 +11,19 @@ public class PlayerMovment : MonoBehaviour
 
     public float jumpPower = 1f;
     public float normalSpeed = 2f;
+    public float minY;
+    public float maxY;
     private Rigidbody2D rb;
     private float speed;
     private Vector2 velocity;
+
+    [Header ("SoundFX")]
+
+    public GameObject destroySound;
+    public AudioSource destroyAudio;
+    public float waitTime;
+    public GameObject playerSprite;
+    public ParticleSystem playerPart;
 
 
     #endregion
@@ -21,20 +31,28 @@ public class PlayerMovment : MonoBehaviour
 
     void Awake()
     {
+        //Set vars
         rb = this.GetComponent<Rigidbody2D>();
         speed = normalSpeed;
+        destroyAudio = destroySound.GetComponent<AudioSource>();
+        playerPart = gameObject.GetComponent<ParticleSystem>();
     }
 
 
 
     void Update()
     {
+        //Process movment
         ProcMovment();
 
     }
 
     void ProcMovment()
     {
+        //Clamp player movment on Y
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, Mathf.Clamp(this.gameObject.transform.position.y,minY,maxY), this.gameObject.transform.position.z);
+        
+        //Input jump
         if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0)){
             velocity = new Vector2(0f,jumpPower);
             rb.AddForce(velocity, ForceMode2D.Impulse);
@@ -43,13 +61,39 @@ public class PlayerMovment : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D other) 
+    void OnCollisionEnter2D(Collision2D other) 
     {
+        //Collision
         if(other.gameObject.tag == "Obstacle"){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            CallDeath();
 
         }
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.tag == "Border"){
+            CallDeath();
+        }
+        
+    }
+
+
+    void CallDeath()
+    {
+        playerSprite.SetActive(false);
+        playerPart.Stop();
+        StartCoroutine(DeathCooldown());
+
+    }
+
+    IEnumerator DeathCooldown()
+    {
+        //Play audio then wait and change scene
+        destroyAudio.PlayOneShot(destroyAudio.clip);
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 
